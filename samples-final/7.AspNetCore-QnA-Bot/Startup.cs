0c5ba5c41f190abd4d64e8fb2679cba39ffd72a7
Tony Jeng <jeng.tony@aaa-calif.com>
2018-08-14 14:09:55 -0700

    Add in setup for publish and deploy to azure and beginning of Rich cards

--- samples-final/7.AspNetCore-QnA-Bot/Startup.cs
+++ samples-final/7.AspNetCore-QnA-Bot/Startup.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder.BotFramework;
@@ -11,6 +12,7 @@ using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Builder.TraceExtensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AspNetCore_RichCards_Bot;
 
 namespace AspNetCore_QnA_Bot
 {
@@ -53,6 +55,41 @@ namespace AspNetCore_QnA_Bot
                 var qnaMiddleware = new QnAMakerMiddleware(qnaEndpoint);
 
                 options.Middleware.Add(qnaMiddleware);
+            });
+
+            services.AddBot<RichCardsBot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(Configuration);
+
+                // The CatchExceptionMiddleware provides a top-level exception handler for your bot. 
+                // Any exceptions thrown by other Middleware, or by your OnTurn method, will be 
+                // caught here. To facillitate debugging, the exception is sent out, via Trace, 
+                // to the emulator. Trace activities are NOT displayed to users, so in addition
+                // an "Ooops" message is sent. 
+                options.Middleware.Add(new CatchExceptionMiddleware<Exception>(async (context, exception) =>
+                {
+                    await context.TraceActivity("EchoBot Exception", exception);
+                    await context.SendActivity("Sorry, it looks like something went wrong!");
+                }));
+
+                // The Memory Storage used here is for local bot debugging only. When the bot
+                // is restarted, anything stored in memory will be gone. 
+                IStorage dataStore = new MemoryStorage();
+
+                // The File data store, shown here, is suitable for bots that run on 
+                // a single machine and need durable state across application restarts.                 
+                // IStorage dataStore = new FileStorage(System.IO.Path.GetTempPath());
+
+                // For production bots use the Azure Table Store, Azure Blob, or 
+                // Azure CosmosDB storage provides, as seen below. To include any of 
+                // the Azure based storage providers, add the Microsoft.Bot.Builder.Azure 
+                // Nuget package to your solution. That package is found at:
+                //      https://www.nuget.org/packages/Microsoft.Bot.Builder.Azure/
+
+                // IStorage dataStore = new Microsoft.Bot.Builder.Azure.AzureTableStorage("AzureTablesConnectionString", "TableName");
+                // IStorage dataStore = new Microsoft.Bot.Builder.Azure.AzureBlobStorage("AzureBlobConnectionString", "containerName");
+
+                options.Middleware.Add(new ConversationState<Dictionary<string, object>>(dataStore));
             });
         }
 
